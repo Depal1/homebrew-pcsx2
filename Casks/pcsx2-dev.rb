9b44bf6664b32ee879220fea531740ff7e5a2fd3
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-03-26 21:54:20 -0600

    livecheck fix

--- Casks/pcsx2-dev.rb
+++ Casks/pcsx2-dev.rb
@@ -10,7 +10,7 @@ cask "pcsx2-dev" do
 
   livecheck do
     url "https://github.com/tellowkrinkle/pcsx2/releases/"
-    regex(%r{/href=.*?PCSX2[._-]v?(\d+(?:\.\d+)+-dev-[0-9]{8}\b)$}/i)
+    regex(%r{/href=.*?PCSX2[._-]v?(\d+(?:\.\d+)+-dev-[0-9]{8}\b)$}i)
   end
 
   app "PCSX2.app"
