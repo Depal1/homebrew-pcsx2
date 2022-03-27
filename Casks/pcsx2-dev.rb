c905f58885e3f1f6178f47c8ec9a9318d545a0e0
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-03-26 20:04:24 -0600

    fix sha256, regex experiment

--- Casks/pcsx2-dev.rb
+++ Casks/pcsx2-dev.rb
@@ -1,6 +1,6 @@
 cask "pcsx2-dev" do
   version "1.7-Dev-2022-03-25"
-  sha256 "4c9a2f1db73fe6a3e4b18b6dadb909d2fcf363914b0ce352419d007e9b1a61ce"
+  sha256 "6c5450718f88c6ff13613385fcac91e1c907aad707c3777463e74a2138eaf66e"
 
   url "https://github.com/tellowkrinkle/pcsx2/releases/download/macos-20220325/PCSX2-1.7.0-dev-20220325-Mac-64Bit.tar.gz",
       verified: "https://github.com/tellowkrinkle/pcsx2/releases"
@@ -9,7 +9,7 @@ cask "pcsx2-dev" do
   homepage "hhttps://pcsx2.net"
 
   livecheck do
-    regex(%r{gh-release/(.*)}i)
+    regex(%r{PCSX2-/v?(\d+(?:\.\d+))
   end
 
   app "PCSX2.app"
