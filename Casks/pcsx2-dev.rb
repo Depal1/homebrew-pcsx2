9acc7fb6d12d90db50ec026c8871c88ef971024e
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-03-26 21:01:47 -0600

    fix regex

--- Casks/pcsx2-dev.rb
+++ Casks/pcsx2-dev.rb
@@ -1,5 +1,5 @@
 cask "pcsx2-dev" do
-  version "1.7-Dev-2022-03-25"
+  version "1.7.0-Dev-2022-03-25"
   sha256 "6c5450718f88c6ff13613385fcac91e1c907aad707c3777463e74a2138eaf66e"
 
   url "https://github.com/tellowkrinkle/pcsx2/releases/download/macos-20220325/PCSX2-1.7.0-dev-20220325-Mac-64Bit.tar.gz",
@@ -10,7 +10,7 @@ cask "pcsx2-dev" do
 
   livecheck do
     url "https://github.com/tellowkrinkle/pcsx2/releases"
-    regex(%r{PCSX2-/v?(\d+(?:\.\d+)+)-dev-[0-9]{8}$}i)
+    regex(%r{PCSX2-/v?(\d+(?:\.\d+)+-dev-[0-9]{8}\b)$}i)
   end
 
   app "PCSX2.app"
