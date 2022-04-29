b398997341c80abc77d8ba26d2c8ead151ce4ce5
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-04-29 16:25:23 -0600

    "change to official nightly release"

---
+++ Casks/pcsx2-nightly.rb
@@ -0,0 +1,26 @@
+cask "pcsx2-nightly" do
+  version "1.7.2676"
+  sha256 "0351bd49af1ec8907441d2a664f952177c55071432cf29a352926200ee390222"
+
+  url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macOS.tar.gz",
+      verified: "https://github.com/PCSX2/pcsx2/releases"
+  name "PCSX2"
+  desc "Playstation 2 Emulator"
+  homepage "hhttps://pcsx2.net"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  app "PCSX2.app"
+
+  zap trash: [
+    "~/Library/Application Support/PCSX2",
+    "~/Library/Saved Application State/net.pcsx2.pcsx2.savedState",
+  ]
+
+  zap rmdir: [
+    "~/Library/Application Support/PCSX2",
+  ]
+end
